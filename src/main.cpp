b8ea0275cad564e414cf6e5dfe0cf8d80d11a483
p0zitive <daniel@gmail.com>
2026-07-25 20:24:01 +0300

    Refactor of map drawing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "MapDrawingData.h"
 
 constexpr int TILE_WIDTH = 16;
 constexpr int TILE_HEIGHT = 16;
@@ -8,101 +9,114 @@ constexpr int WINDOW_OFFSET = 1;
 constexpr int WINDOW_WIDTH = (MAP_SIZE + WINDOW_OFFSET) * TILE_WIDTH * ZOOM_LEVEL;
 constexpr int WINDOW_HEIGHT = (MAP_SIZE + WINDOW_OFFSET) * TILE_HEIGHT * ZOOM_LEVEL;
 
+// TODO: Add level procedural generation in the future
+void MapDrawing(MapDrawingData data);
+
+MapDrawingData CreateMapDrawingData();
+
 int main(void) {
     // TODO: In the future add a function to initialize window properties based on user configs
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Raylib Dungeon");
-    Texture2D tileset = LoadTexture("resources/tilemap.png");
 
-    Rectangle floorTile = {160, 192, TILE_WIDTH, TILE_HEIGHT};
-
-    Rectangle topLeftCorner = {80, 112, TILE_WIDTH, TILE_HEIGHT};
-    Rectangle topRightCorner = {112, 112, TILE_WIDTH, TILE_HEIGHT};
-    Rectangle bottomLeftCorner = {80, 144, TILE_WIDTH, TILE_HEIGHT};
-    Rectangle bottomRightCorner = {112, 144, TILE_WIDTH, TILE_HEIGHT};
-
-    Rectangle topWall = {96, 112, TILE_WIDTH, TILE_HEIGHT};
-    Rectangle leftWall = {80, 128, TILE_WIDTH, TILE_HEIGHT};
-    Rectangle rightWall = {112, 128, TILE_WIDTH, TILE_HEIGHT};
-    Rectangle bottomWall = {96, 144, TILE_WIDTH, TILE_HEIGHT};
-
+    MapDrawingData data = CreateMapDrawingData();
+    
     Camera2D camera;
     camera.target = {0, 0};
     camera.offset = {0, 0};
     camera.rotation = 0.0f;
     camera.zoom = ZOOM_LEVEL;
-
+    
     while (!WindowShouldClose()) {
         BeginDrawing();
         {
             ClearBackground(BLACK);
             BeginMode2D(camera);
             {
-                for (int i = WINDOW_OFFSET; i < MAP_SIZE + WINDOW_OFFSET; i++) {
-                    for (int j = WINDOW_OFFSET; j < MAP_SIZE + WINDOW_OFFSET; j++) {
-                        const float x = static_cast<float>(i * TILE_WIDTH);
-                        const float y = static_cast<float>(j * TILE_HEIGHT);
-                        const Vector2 tilePos = {x, y};
-
-                        // top left corner
-                        if (i == WINDOW_OFFSET && j == WINDOW_OFFSET) {
-                            DrawTextureRec(tileset, topLeftCorner, tilePos, WHITE);
-                            continue;
-                        }
-
-                        // left wall
-                        if (i == WINDOW_OFFSET && j > WINDOW_OFFSET && j < MAP_SIZE - 1) {
-                            DrawTextureRec(tileset, leftWall, tilePos, WHITE);
-                            continue;
-                        }
-
-                        // bottom left corner
-                        if (i == WINDOW_OFFSET && j == MAP_SIZE - 1) {
-                            DrawTextureRec(tileset, bottomLeftCorner, tilePos, WHITE);
-                            continue;
-                        }
-
-                        // top wall
-                        if ((i > WINDOW_OFFSET && i < MAP_SIZE - 1) && j == WINDOW_OFFSET) {
-                            DrawTextureRec(tileset, topWall, tilePos, WHITE);
-                            continue;
-                        }
-
-                        // top right corner
-                        if (i == MAP_SIZE - 1 && j == WINDOW_OFFSET) {
-                            DrawTextureRec(tileset, topRightCorner, tilePos, WHITE);
-                            continue;
-                        }
-
-                        // right wall
-                        if (i == MAP_SIZE - 1 && (j > WINDOW_OFFSET && j < MAP_SIZE - 1)) {
-                            DrawTextureRec(tileset, rightWall, tilePos, WHITE);
-                            continue;
-                        }
-
-                        // bottom right corner
-                        if (i == MAP_SIZE - 1 && j == MAP_SIZE - 1) {
-                            DrawTextureRec(tileset, bottomRightCorner, tilePos, WHITE);
-                            continue;
-                        }
-
-                        // bottom wall
-                        if ((i > WINDOW_OFFSET && i < MAP_SIZE) && j == MAP_SIZE - 1) {
-                            DrawTextureRec(tileset, bottomWall, tilePos, WHITE);
-                            continue;
-                        }
-
-                        // all floor tiles
-                        if ((i > WINDOW_OFFSET && i < MAP_SIZE) && (j > WINDOW_OFFSET && j < MAP_SIZE)) {
-                            DrawTextureRec(tileset, floorTile, tilePos, WHITE);
-                        }
-                    }
-                }
+                MapDrawing(data);
             }
             EndMode2D();
         }
         EndDrawing();
     }
-    UnloadTexture(tileset);
+    UnloadTexture(data.tileset);
     CloseWindow();
     return 0;
+}
+
+void MapDrawing(MapDrawingData data) {
+    for (int i = WINDOW_OFFSET; i < MAP_SIZE + WINDOW_OFFSET; i++) {
+        for (int j = WINDOW_OFFSET; j < MAP_SIZE + WINDOW_OFFSET; j++) {
+            const float x = static_cast<float>(i * TILE_WIDTH);
+            const float y = static_cast<float>(j * TILE_HEIGHT);
+            const Vector2 tilePos = {x, y};
+
+            // top left corner
+            if (i == WINDOW_OFFSET && j == WINDOW_OFFSET) {
+                DrawTextureRec(data.tileset, data.topLeftCorner, tilePos, WHITE);
+                continue;
+            }
+
+            // left wall
+            if (i == WINDOW_OFFSET && j > WINDOW_OFFSET && j < MAP_SIZE - 1) {
+                DrawTextureRec(data.tileset, data.leftWall, tilePos, WHITE);
+                continue;
+            }
+
+            // bottom left corner
+            if (i == WINDOW_OFFSET && j == MAP_SIZE - 1) {
+                DrawTextureRec(data.tileset, data.bottomLeftCorner, tilePos, WHITE);
+                continue;
+            }
+
+            // top wall
+            if ((i > WINDOW_OFFSET && i < MAP_SIZE - 1) && j == WINDOW_OFFSET) {
+                DrawTextureRec(data.tileset, data.topWall, tilePos, WHITE);
+                continue;
+            }
+
+            // top right corner
+            if (i == MAP_SIZE - 1 && j == WINDOW_OFFSET) {
+                DrawTextureRec(data.tileset, data.topRightCorner, tilePos, WHITE);
+                continue;
+            }
+
+            // right wall
+            if (i == MAP_SIZE - 1 && (j > WINDOW_OFFSET && j < MAP_SIZE - 1)) {
+                DrawTextureRec(data.tileset, data.rightWall, tilePos, WHITE);
+                continue;
+            }
+
+            // bottom right corner
+            if (i == MAP_SIZE - 1 && j == MAP_SIZE - 1) {
+                DrawTextureRec(data.tileset, data.bottomRightCorner, tilePos, WHITE);
+                continue;
+            }
+
+            // bottom wall
+            if ((i > WINDOW_OFFSET && i < MAP_SIZE) && j == MAP_SIZE - 1) {
+                DrawTextureRec(data.tileset, data.bottomWall, tilePos, WHITE);
+                continue;
+            }
+
+            // all floor tiles
+            if ((i > WINDOW_OFFSET && i < MAP_SIZE) && (j > WINDOW_OFFSET && j < MAP_SIZE)) {
+                DrawTextureRec(data.tileset, data.floorTile, tilePos, WHITE);
+            }
+        }
+    }
+}
+
+MapDrawingData CreateMapDrawingData() {
+    MapDrawingData data;
+    data.tileset = LoadTexture("resources/tilemap.png");
+    data.floorTile = {160, 192, TILE_WIDTH, TILE_HEIGHT};
+    data.topLeftCorner = {80, 112, TILE_WIDTH, TILE_HEIGHT};
+    data.topRightCorner = {112, 112, TILE_WIDTH, TILE_HEIGHT};
+    data.bottomLeftCorner = {80, 144, TILE_WIDTH, TILE_HEIGHT};
+    data.bottomRightCorner = {112, 144, TILE_WIDTH, TILE_HEIGHT};
+    data.topWall = {96, 112, TILE_WIDTH, TILE_HEIGHT};
+    data.leftWall = {80, 128, TILE_WIDTH, TILE_HEIGHT};
+    data.rightWall = {112, 128, TILE_WIDTH, TILE_HEIGHT};
+    data.bottomWall = {96, 144, TILE_WIDTH, TILE_HEIGHT};
+    return data;
 }
---
+++ src/mapDrawingData.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+struct MapDrawingData {
+    Texture2D tileset;
+    Rectangle floorTile;
+    Rectangle topLeftCorner;
+    Rectangle topRightCorner;
+    Rectangle bottomLeftCorner;
+    Rectangle bottomRightCorner;
+    Rectangle topWall;
+    Rectangle leftWall;
+    Rectangle rightWall;
+    Rectangle bottomWall;
+};
